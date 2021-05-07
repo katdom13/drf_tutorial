18a04ac5da4e449811ea20c02fbb1571887369ff
katdom13 <katdom13@gmail.com>
2021-05-08 05:16:28 +0800

    MINOR CHANGES: Corrected spelling, updated view arrangement

--- snippets/urls.py
+++ snippets/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.api_root),
     path('snippets/', views.SnippetListView.as_view(), name='snippet-list'),
     path('snippets/<int:pk>/', views.SnippetDetailView.as_view(), name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', views.SnipperHighlightView.as_view(), name='snippet-highlight'),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlightView.as_view(), name='snippet-highlight'),
     path('users/', views.UserListView.as_view(), name='user-list'),
     path('users/<int:pk>', views.UserDetailView.as_view(), name='user-detail'),
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -43,6 +43,23 @@ class SnippetDetailView(generics.RetrieveUpdateDestroyAPIView):
     ]
 
 
+class SnippetHighlightView(generics.GenericAPIView):
+    """
+    HTML endpoint for highlighted snippets
+    """
+    queryset = Snippet.objects.all()
+
+    # Render pre-rendered HTML
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        """
+        Custom get method that returns an object ATTRIBUTE
+        """
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
 class UserListView(generics.ListAPIView):
     """
     Read-only list view for the user representation
@@ -67,21 +84,3 @@ def api_root(request, format=None):
         'users': reverse('user-list', request=request, format=format),
         'snippets': reverse('snippet-list', request=request, format=format),
     })
-
-
-
-class SnipperHighlightView(generics.GenericAPIView):
-    """
-    HTML endpoint for highlighted snippets
-    """
-    queryset = Snippet.objects.all()
-
-    # Render pre-rendered HTML
-    renderer_classes = [renderers.StaticHTMLRenderer]
-
-    def get(self, request, *args, **kwargs):
-        """
-        Custom get method that returns an object ATTRIBUTE
-        """
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
